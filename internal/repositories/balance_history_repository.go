2cb06f5ddd18827498991cf5800f3b93b90cce7f
Zhumabekov04 <alim.zhumabekov@bk.ru>
2024-09-27 11:35:07 +0500

    balance history

--- internal/repositories/balance_history_repository.go
+++ internal/repositories/balance_history_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"tender/internal/models"
 )
 
@@ -22,6 +23,7 @@ func (r *BalanceHistoryRepository) CreateBalanceHistory(ctx context.Context, his
 	if err != nil {
 		return models.BalanceHistory{}, err
 	}
+	fmt.Println(id)
 
 	rows, err := r.Db.QueryContext(ctx, "SELECT id, amount, description, user_id, created_at, updated_at FROM balance_history WHERE id = ?", id)
 	if err != nil {
