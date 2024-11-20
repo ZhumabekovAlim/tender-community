5dcb516a88de1e9e8db4bf03b2649773d24cf9be
Zhumabekov04 <alim.zhumabekov@bk.ru>
2024-11-20 17:08:43 +0500

    tenders

--- internal/repositories/tender_repository.go
+++ internal/repositories/tender_repository.go
@@ -126,14 +126,14 @@ func (r *TenderRepository) GetTenderByID(ctx context.Context, id int) (models.Te
 	var tender models.Tender
 	err := r.Db.QueryRowContext(ctx, `
         SELECT tenders.id, type, tender_number, user_id, company_id, organization,
-               total, commission, completed_date, date, tenders.status, u.name, c.name
+               total, commission, completed_date, date,  u.name, c.name
         FROM tenders
         JOIN tender.users u ON u.id = tenders.user_id
 		JOIN tender.companies c ON c.id = tenders.company_id
 		 WHERE tenders.id = ?
 		ORDER BY tenders.date DESC`, id).Scan(
 		&tender.ID, &tender.Type, &tender.TenderNumber, &tender.UserID, &tender.CompanyID, &tender.Organization,
-		&tender.Total, &tender.Commission, &tender.CompletedDate, &tender.Date, &tender.Status, &tender.UserName, &tender.CompanyName,
+		&tender.Total, &tender.Commission, &tender.CompletedDate, &tender.Date, &tender.UserName, &tender.CompanyName,
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
